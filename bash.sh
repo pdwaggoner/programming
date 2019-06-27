144b1c6f43eb80e01fe5cea1de4b71e221f90739
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2019-06-27 15:06:55 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,21 +5,17 @@
 
 # Next, basic command is to echo (`print` in R and Python; `return` in R)
 # Note the "" = verbatim character return
-
 osx:~ $ echo "hello world"
 
 # Next we can find our working directory (`pwd`) and list it's contents (`ls`)
-
 osx:~ $ pwd
 
 osx:~ $ ls
 
 # We can also change the working directory with `cd`, followed by the new location (here, `examples/`)
-
 osx:~ $ cd examples/
 
 # Other useful commands: `mkdir` (make new directory); `mv` (move file); `()` and `{}` (for writing new functions)
-
 osx:examples $ mkdir newex
 
 osx:myproject $ mv ../newex.txt ./
