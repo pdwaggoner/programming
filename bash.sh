b95de5c3641f7c214bb4f9b24710b60bce14f830
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2019-06-27 11:54:04 -0400

    Update and rename bash to bash.sh

---
+++ bash.sh
@@ -0,0 +1,46 @@
+# We need to start with setting up a directory and manipulating files. 
+
+# `osx:~ $` is the prompt; `$`, is the command; `#` is the comment
+
+# This basic script greatly benefitted from Jake VanderPlas and "Software Carpentry"
+
+# First tell the terminal we are using a "bash" shell
+
+``` bash
+#!/bin/bash                            
+```
+
+# Next, basic command is to echo (`print` in R and Python; `return` in R)
+# Note the "" == verbatim character return
+
+```bash
+osx:~ $ echo "hello world"
+```
+
+# Next we can find our working directory (`pwd`) and list it's contents (`ls`)
+
+``` bash
+osx:~ $ pwd
+
+osx:~ $ ls
+```
+
+# We can also change the working directory with `cd`, followed by the new location (here, `examples/`)
+
+``` bash
+osx:~ $ cd examples/
+```
+
+# Other useful commands: `mkdir` (make new directory); `mv` (move file); `()` and `{}` (for writing new functions)
+
+``` bash
+osx:examples $ mkdir newex
+
+osx:myproject $ mv ../newex.txt ./
+
+_simple_function(){
+  echo "This is a function."
+  echo "Now exiting."
+}
+_simple_function
+```
